4d4503fb184423f623b71de2ea5b9062af4798d1
Savannah Smith <savsmith@utexas.edu>
2021-05-06 23:36:02 -0500

    midnight adding

--- source/api.py
+++ source/api.py
@@ -20,20 +20,60 @@ def jobs_api():
         return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
     return json.dumps(jobs.add_job(job['start'], job['end']))
 
-@app.route('/', methods = ['GET'])
-def get_animals():
-	return json.dumps(get_data())
+#create
+@app.route('/generate', methods=['GET'])
+def generate():
+    unemployment_data = {}
+    unemployment_data['Year'] = 
+    unemployment_data['Area'] = 
+    unemployment_data['Employment'] = 
+    unemployment_data['Civilian Labor Force'] = 
+    unemployment_data['Unemployment'] = 
+    unemployment_data['Unemployment Rate'] = 
+ 
+    rd.hmset(str(uuid.uuid4()), this_animal)
+  
+    return '20 animals have been generated'
 
-@app.route('/animals/arms/<arm_num>', methods = ['GET'])
-def get_animals_arms(arm_num):
+#read
+@app.route('/animals/created_on/<string:s_date>/<string:e_date>', methods = ['GET'])
+def get_animals(s_date,e_date):
 	test = get_data()
-	return json.dumps([x for x in test if x['arms'] == arm_num])
+	animals_r = data['animals']
+	list1 = []
+	start = datetime.strptime(s_date, '%Y-%m-%d')
+	end = datetime.strptime(e_date, '%Y-%m-%d')
+	for animal in animals:
+		date = datetime.strptime(animal['created_on'], '%Y-%m-%d')
+		if ((date>=start) and (date<=end)):
+			list1.append(animal)
+	return jsonify({'animals in range': list1})
 
-@app.route('/animals/head/<head>', methods = ['GET'])
-def get_animals_head(head):
+#update
+@app.route('/animals/mod/<string:sel_uid>', methods = ['PUT'])
+def mod_animals_uuid(sel_uid):
 	test = get_data()
-	return json.dumps([x for x in test if x['head'] == head])
-
+	animals_m = data['animals']
+	for animal in animals_m:
+                if (animal['uid'] == sel_uid):
+			tail = animal['tail']
+			animal['tail'] = tail + 2
+                        return jsonify ({'modified animal':animal})
+        	else :
+                	return jsonify ({'animal': 'animal does not exist'})
+#delete
+@app.route('/delete/<string:s_date>/<string:e_date>', methods = ['GET'])
+def del_animals(s_date,e_date):
+	test = get_data()
+	unemployment_data_d = data['unemployment_data']
+	list2 = []
+        start = datetime.strptime(s_date, '%Y-%m-%d')
+        end = datetime.strptime(e_date, '%Y-%m-%d')
+	for animal in animals_d:
+                date = datetime.strptime(animal['created_on'], '%Y-%m-%d')
+                if ((date<start) or (date>end)):
+                        list2.append(animal)
+        return jsonify({'Unemployment data without data in range': list2})
 
 
 if __name__ == '__main__':
--- source/worker.py
+++ source/worker.py
@@ -1,14 +1,37 @@
 from jobs import q, update_job_status
-import time
+import matplotlib.pyplot as plt
+import numpy as np
+import redis
+
+rd = redis.StrictRedis(host='172.17.0.1', port=6379, db=0)
 
 @q.worker
 def execute_job(jid):
     jobs.update_job_status(jid, 'in progress')
+   
+    X = np.arange(10)
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    plt.xlabel('Years')
+    plt.ylabel('Unemployment Rates')
+    ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
+    ax.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+    ax.bar(X + 0.50, data[2], color = 'y', width = 0.25)
+    ax.bar(X + 0.75, data[3], color = 'r', width = 0.25)
+    
+    plt.show()
+    plt.savefig('my_fig.png')
 
-#something happens here
+   """ years =
+    regions =
+    unemployment_rates ="""
 
 
+    # read the raw file bytes into a python object
+    file_bytes = open('/tmp/my_fig.png', 'rb').read()
 
+    # set the file bytes as a key in Redis
+    rd.set('key', file_bytes)
 
 
     jobs.update_job_status(jid, 'complete')
